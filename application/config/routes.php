f8a90ced3ff6489a2af98cb335779ab2c68523bd
Sriram Patibanda <patibandasriram2k02@gmail.com>
2020-06-22 03:14:11 +0530

    adding categories

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,6 +7,10 @@ $route['posts/(:any)'] = 'posts/view/$1';
 $route['posts'] = 'posts/index';
 
 $route['default_controller'] = 'pages/view';
+
+$route['categories'] = 'categories/index';
+$route['categories/create'] = 'categories/create';
+$route['categories/posts/(:any)'] = 'categories/posts/$1';
 $route['(:any)'] = 'pages/view/';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/categories/create.php
@@ -0,0 +1,14 @@
+<div class="post-body">
+<h2><?= $title ;?></h2>
+
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open_multipart('categories/create'); ?>
+    <div class="form-group">
+    <label>Name</label>
+    <input type="text" class="form-control" name="name" placeholder="Enter name">
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+</form>
+</div>
